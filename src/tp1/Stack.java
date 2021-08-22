518144fe2f9272c16435f652d8d7f58867f62f64
Mauricio Oruezabal <moruezabal@gmail.com>
2021-08-22 11:39:02 -0300

    start Stack exercise

---
+++ src/tp1/Stack.java
@@ -0,0 +1,27 @@
+package tp1;
+
+public class Stack<T> {
+	private MySimpleLinkedList<T> list;
+	
+	public Stack(){
+		this.list = null;
+	}
+	
+	public void push(T o) {
+		//TODO
+	};
+	
+	public T pop() {
+		//TODO
+		return null;	
+	}
+	
+	public T top() {
+		//TODO
+		return null;
+	}
+
+	public void reverse() {
+		//TODO
+	}
+}
