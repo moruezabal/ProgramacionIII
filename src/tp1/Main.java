c0de14f80abbcd22492d3d77215b2525190021bd
Mauricio Oruezabal <moruezabal@gmail.com>
2021-08-26 09:53:51 -0300

    P1E7 exercise complete

--- src/tp1/Main.java
+++ src/tp1/Main.java
@@ -5,22 +5,18 @@ public class Main {
 	public static void main(String[] args) {
 			
 		MySimpleLinkedList<Integer> listaNum1 = new MySimpleLinkedList<Integer>();
-		listaNum1.insertLast(9);
-		listaNum1.insertLast(3);
-		listaNum1.insertLast(5);
-		listaNum1.insertLast(4);
-		listaNum1.insertLast(2);
+		for(int i = 0; i < 5; i++) {
+			listaNum1.insertLast((int)Math.floor(Math.random()*(10)+1));
+		}
 		System.out.println(listaNum1.toString());
 		
 		MySimpleLinkedList<Integer> listaNum2 = new MySimpleLinkedList<Integer>();
-		listaNum2.insertLast(8);
-		listaNum2.insertLast(5);
-		listaNum2.insertLast(4);
-		listaNum2.insertLast(6);
-		listaNum2.insertLast(9);
+		for(int i = 0; i < 5; i++) {
+			listaNum2.insertLast((int)Math.floor(Math.random()*9));
+		}
 		System.out.println(listaNum2.toString());
 		
-		System.out.println(listaEnComunDesord(listaNum1, listaNum2).toString());	
+		System.out.println(primeraSiSegundaNo(listaNum1, listaNum2).toString());	
 	}
 	
 	public static MySimpleLinkedList<Integer> listaEnComunOrd(MySimpleLinkedList<Integer> l1, MySimpleLinkedList<Integer>l2){
@@ -58,4 +54,26 @@ public class Main {
 		return result;
 	}
 	
+	public static MySimpleLinkedList<Integer> primeraSiSegundaNo(MySimpleLinkedList<Integer> l1, MySimpleLinkedList<Integer>l2){
+		MySimpleLinkedList<Integer> result = new MySimpleLinkedList<Integer>();
+		MyIterator<Integer> it1 = l1.iterator();
+		MyIterator<Integer> it2 = l2.iterator();
+		
+		while(it1.hasNext()) {
+			int num1 = it1.next();
+			boolean match = false;
+			while(!match && it2.hasNext()) {
+				int num2 = it2.next();
+				if(num1 == num2) {
+					match = true;
+					it2 = l2.iterator();
+				}
+			}
+			if (!match) {
+				result.insertLast(num1);
+			}
+			it2 = l2.iterator();
+		}
+		return result;
+	}
 }
