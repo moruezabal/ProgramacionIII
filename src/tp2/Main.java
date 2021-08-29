b3273a0ac5f6b0799cf4e0296e03fd761f1c6df9
Mauricio Oruezabal <moruezabal@gmail.com>
2021-08-29 11:27:43 -0300

    TP2E2 complete

--- src/tp2/Main.java
+++ src/tp2/Main.java
@@ -4,8 +4,16 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		int [] numeros = {3,3,4,6,9}; 
-		System.out.println(estaOrdenado(numeros));
+//		int [] numeros = {3,3,4,6,9}; 
+//		System.out.println(estaOrdenado(numeros));
+		
+		MySimpleLinkedList<Integer> listaNum1 = new MySimpleLinkedList<Integer>();
+		for(int i = 0; i < 10; i++) {
+			listaNum1.insertLast((int)Math.floor(Math.random()*9));
+		}
+		System.out.println(listaNum1.toString());
+		
+		System.out.println(findElementIT(listaNum1,4));
 	}
 	
 	public static boolean estaOrdenado(int array[]) {
@@ -22,7 +30,42 @@ public class Main {
 				ordenado = false;
 		}	
 		return ordenado;
-	};
+	}
+	
+	public static int findElement(MySimpleLinkedList<Integer> list, int num) {
+		return findElement(list, num, 0);
+	}
+	
+	private static int findElement(MySimpleLinkedList<Integer> list, int num, int index) {
+		int find = -1;
+		if (index < list.size()) {
+			if(list.get(index) != num) // Atención: El get recorre itera siempre desde el primer elemento, aumentando la complejidad
+				find = findElement(list, num, index+1);
+			else
+				find = index;
+		}
+		return find;
+	}
+	
+	public static int findElementIT(MySimpleLinkedList<Integer> list, int num) {
+		MyIterator<Integer> it1 = list.iterator();
+		return findElementIT(list, num, it1, 0);
+	}
+	
+	private static int findElementIT(MySimpleLinkedList<Integer> list, int num, MyIterator<Integer> it, int index) {
+		int find = -1;
+		if (it.hasNext()) {
+			if(it.next() != num) {
+				index++;
+				find = findElementIT(list, num, it, index);
+			}
+			else {
+				find = index;
+			}
+		}
+		
+		return find;
+	}
 	
 	
 }
