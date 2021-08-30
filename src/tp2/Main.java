42d2de879e1a3299fbea0cf2bf542eaf4f0b7588
Mauricio Oruezabal <moruezabal@gmail.com>
2021-08-30 12:15:30 -0300

    P2E4 & P2E5 complete

--- src/tp2/Main.java
+++ src/tp2/Main.java
@@ -17,7 +17,12 @@ public class Main {
 //		int [] numbers = {2,4,5,7,9}; 
 //		System.out.println(findElementORD(numbers, 7));
 		
-		System.out.println(toBinary(82));
+//		System.out.println(toBinary(92));
+		
+		int [] numbers = {5,8,1,7,0,3};
+		quicksort(numbers);
+		for (int number : numbers)
+			System.out.print(number + " ");
 	}
 	
 	public static boolean estaOrdenado(int array[]) {
@@ -99,5 +104,149 @@ public class Main {
 		return binary;
 	}
 	
+	public static void burbujeo(int[] A) { // O(n2)
+        int i, j, aux;
+        for (i = 0; i < A.length - 1; i++) {
+            for (j = 0; j < A.length - i - 1; j++) {
+                if (A[j + 1] < A[j]) {
+                    aux = A[j + 1];
+                    A[j + 1] = A[j];
+                    A[j] = aux;
+                }
+            }
+        }
+	}
 	
+	public static void seleccion(int A[]) { // O(n2)
+      int i, j, menor, pos, tmp;
+      for (i = 0; i < A.length - 1; i++) {      
+            menor = A[i];                                         
+            pos = i;                            
+            for (j = i + 1; j < A.length; j++){ 
+                  if (A[j] < menor) {           
+                      menor = A[j];             
+                      pos = j;
+                  }
+            }
+            if (pos != i){                                        
+                tmp = A[i];
+                A[i] = A[pos];
+                A[pos] = tmp;
+            }
+      }
+	}
+	
+	public static int[] mergeSort(int a[]) { // O(n.log n)
+		return divide(a, a.length);
+		}
+
+	public static int[] divide(int a[], int n) {
+		if (n > 1) {
+			if (n % 2 == 0) {
+				int a1[] = new int[n / 2];
+				int a2[] = new int[n / 2];
+				
+				for (int i = 0; i < n; i++) {
+					if (i < n / 2) {
+						a1[i] = a[i];
+					} else {
+						int ind = i - (n / 2);
+						a2[ind] = a[i];
+					}
+				}
+				return combina(divide(a1, n / 2), divide(a2, n / 2), n);
+			} else {
+				int a1[] = new int[(n - 1) / 2];
+				int a2[] = new int[(n + 1) / 2];
+				
+				for (int j = 0; j < n; j++) {
+					if (j < (n - 1) / 2) {
+						a1[j] = a[j];
+					} else {
+						int ind = j - ((n - 1) / 2);
+						a2[ind] = a[j];
+					}
+				}
+				return combina(divide(a1, (n - 1) / 2), divide(a2, (n + 1) / 2), n);
+			}
+		} else {
+			return a;
+		}
+	}
+
+	public static int[] combina(int a1[], int a2[], int n) {
+		int i = 0, j = 0, k = 0;
+		int z[] = new int[n];
+		
+		if (n % 2 == 0) {
+			while (k < n) {
+				if (i >= (n / 2)) {
+					z[k] = a2[j];
+					j++;
+				} else if (j >= (n / 2)) {
+					z[k] = a1[i];
+					i++;
+				} else if (a1[i] <= a2[j]) {
+					z[k] = a1[i];
+					i++;
+				} else {
+					z[k] = a2[j];
+					j++;
+				}
+					k++;
+			}
+			return z;
+		} else {
+			while (k < n) {
+				if (i >= ((n - 1) / 2)) {
+					z[k] = a2[j];
+					j++;
+				} else if (j >= ((n + 1) / 2)) {
+					z[k] = a1[i];
+					i++;
+				} else if (a1[i] <= a2[j]) {
+					z[k] = a1[i];
+					i++;
+				} else {
+					z[k] = a2[j];
+					j++;
+				}
+				k++;
+			}
+			return z;
+		}
+	}
+	
+	//En el peor caso, cuando el pivote es el elemento menor del array el tiempo de ejecución del método Quicksort es O(n2).
+	//En general el tiempo medio de ejecución del Quicksort es O(n log n).
+	public static void quicksort(int A[]) { 
+		quicksort(A, 0, A.length -1);
+	}
+	
+	private static void quicksort(int A[], int izq, int der) {
+
+	  int pivote=A[izq]; // tomamos primer elemento como pivote
+	  int i=izq;         // i realiza la búsqueda de izquierda a derecha
+	  int j=der;         // j realiza la búsqueda de derecha a izquierda
+	  int aux;
+	 
+	  while(i < j){                          // mientras no se crucen las búsquedas                                   
+	     while(A[i] <= pivote && i < j) i++; // busca elemento mayor que pivote
+	     while(A[j] > pivote) j--;           // busca elemento menor que pivote
+	     if (i < j) {                        // si no se han cruzado                      
+	         aux= A[i];                      // los intercambia
+	         A[i]=A[j];
+	         A[j]=aux;
+	     }
+	  }
+	   
+	   A[izq]=A[j];      // se coloca el pivote en su lugar de forma que tendremos                                    
+	   A[j]=pivote;      // los menores a su izquierda y los mayores a su derecha
+	   
+	   if(izq < j-1)
+	      quicksort(A,izq,j-1);          // ordenamos subarray izquierdo
+	   if(j+1 < der)
+	      quicksort(A,j+1,der);          // ordenamos subarray derecho
+	}
+
 }
